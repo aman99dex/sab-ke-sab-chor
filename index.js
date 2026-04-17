59286437dca77cc92e2c3f862c4630ebcbced49f
Aman <kumarguptaaman896@gmail.com>
2026-04-17 22:49:34 +0530

    fixed data loading bug

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-
-const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
 
 const resolvers = {
   Query: {
-    hello: () => "Hello from GraphQL!",
+    games: () => db.games,
+    game: (_, args) => db.games.find((g) => g.id === args.id),
+    reviews: () => db.reviews,
+    review: (_, args) => db.reviews.find((r) => r.id === args.id),
+    authors: () => db.authors,
+    author: (_, args) => db.authors.find((a) => a.id === args.id),
+  },
+  Game: {
+    reviews: (parent) => db.reviews.filter((r) => r.game_id === parent.id),
+  },
+  Author: {
+    reviews: (parent) => db.reviews.filter((r) => r.author_id === parent.id),
+  },
+  Review: {
+    game: (parent) => db.games.find((g) => g.id === parent.game_id),
+    author: (parent) => db.authors.find((a) => a.id === parent.author_id),
+  },
+  Mutation: {
+    addGame: (_, args) => {
+      const game = { ...args.game, id: String(db.games.length + 1) };
+      db.games.push(game);
+      return game;
+    },
+    deleteGame: (_, args) => {
+      db.games = db.games.filter((g) => g.id !== args.id);
+      return db.games;
+    },
+    updateGame: (_, args) => {
+      db.games = db.games.map((g) =>
+        g.id === args.id ? { ...g, ...args.edits } : g
+      );
+      return db.games.find((g) => g.id === args.id);
+    },
   },
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
 
-console.log(`🚀 Server ready at: ${url}`);
+console.log(`Server ready at: ${url}`);
